a610d363a211a7da33b7de1e3e2434c0a6789698
Willa2023 <142073064+Willa2023@users.noreply.github.com>
2024-08-11 18:36:47 +1200

    initial comment

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace AnimalMatchingGame;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public void PlayAgainButton_Clicked(object sender, EventArgs e)
+	{
+		AnimalButtons.IsVisible = true;
+		PlayAgainButton.IsVisible = false;
+
+		List<string> animalEmoji = new List<string> { "🐶", "🐶", "🐱", "🐱", "🐰", "🐰", "🦊", "🦊", "🐻", "🐻", "🐼", "🐼", "🐠", "🐠", "🦉", "🦉" };
+		foreach (var button in AnimalButtons.Children.OfType<Button>())
+		{
+			int index = Random.Shared.Next(animalEmoji.Count);
+			string nextEmoji = animalEmoji[index];
+			button.Text = nextEmoji;
+			animalEmoji.RemoveAt(index);
+		}
+	}
+
+	public void Button_Clicked(object sender, EventArgs e)
+	{
+		Console.WriteLine("Button clicked");
+	}
+
+}
+

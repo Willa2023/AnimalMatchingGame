41a5db7d581f53f7da10ce3b34bbd934798ca3e8
Willa2023 <142073064+Willa2023@users.noreply.github.com>
2024-08-11 19:36:07 +1200

    add match animal logic and TimerTick

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,11 @@
 
 public partial class MainPage : ContentPage
 {
+	Button lastButtonClicked;
+	Boolean findingMatch = false;
+	int matchesFound;
+	int tenthsOfSecondsElapsed = 0;
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -20,11 +25,59 @@ public partial class MainPage : ContentPage
 			button.Text = nextEmoji;
 			animalEmoji.RemoveAt(index);
 		}
+
+		Dispatcher.StartTimer(TimeSpan.FromSeconds(.01), TimerTick);
+	}
+
+	private bool TimerTick()
+	{
+		if (!this.IsLoaded)
+		{
+			return false;
+		}
+		tenthsOfSecondsElapsed++;
+		TimeElapsed.Text = "Time elapsed: " + (tenthsOfSecondsElapsed / 100F).ToString("0.00s");
+
+		if (PlayAgainButton.IsVisible)
+		{
+			tenthsOfSecondsElapsed = 0;
+			return false;
+		}
+
+		return true;
 	}
 
 	public void Button_Clicked(object sender, EventArgs e)
 	{
-		Console.WriteLine("Button clicked");
+		if (sender is Button buttonClicked)
+		{
+			if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+			{
+				buttonClicked.BackgroundColor = Colors.Red;
+				lastButtonClicked = buttonClicked;
+				findingMatch = true;
+			}
+			else
+			{
+				if ((buttonClicked != lastButtonClicked) && (buttonClicked.Text == lastButtonClicked.Text) && (!string.IsNullOrWhiteSpace(buttonClicked.Text)))
+				{
+					matchesFound++;
+					lastButtonClicked.Text = " ";
+					buttonClicked.Text = " ";
+				}
+				lastButtonClicked.BackgroundColor = Colors.AliceBlue;
+				lastButtonClicked.BackgroundColor = Colors.AliceBlue;
+				findingMatch = false;
+			}
+		}
+		if (matchesFound == 8)
+		{
+			matchesFound = 0;
+			AnimalButtons.IsVisible = false;
+			PlayAgainButton.IsVisible = true;
+		}
+
+
 	}
 
 }
